9c7ee3f9a501e5c2ef64e4f64b9c28906d7e7095
HansCYDIa <nothanscydia@proton.me>
2024-09-04 08:48:50 +0800

    Initial Commit

---
+++ MedFix/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MedFix
+//
+//  Created by Hans Camacho on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedFix/MedFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedFixApp.swift
+//  MedFix
+//
+//  Created by Hans Camacho on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MedFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
